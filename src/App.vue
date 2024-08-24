8d760caa1e7ba41ce4be020f9ad2ebc74909ce27
carrotbat410 <carrotbat410@gmail.com>
2024-08-24 14:04:51 +0900

    init vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  beforeCreate() {
+    console.log("LifeCycle is beforeCreate", this.count)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
