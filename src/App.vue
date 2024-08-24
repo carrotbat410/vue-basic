b683492865a52c4efab6c9f917d84065c2f9b5d6
carrotbat410 <carrotbat410@gmail.com>
2024-08-24 14:41:25 +0900

    라이플사이클 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    {{ count }}
-  </div>
+  <div>{{ count }}</div>
+  <h1>Vue.js 라이플사이클 테스트</h1>
 </template>
 
 <script>
@@ -13,6 +12,20 @@ export default {
   },
   beforeCreate() {
     console.log("LifeCycle is beforeCreate", this.count)
+  },
+  created() {
+    console.log("LifeCycle is created", this.count)
+  },
+  beforeMount() {
+    console.log("LifeCycle is beforeMount", document.querySelector('h1'))
+  },
+  mounted() {
+    console.log("LifeCycle is mounted", document.querySelector('h1'))
+  },
+  methods: {
+    test() {
+      console.log("테스트 함수 호출")
+    }
   }
 }
 </script>
